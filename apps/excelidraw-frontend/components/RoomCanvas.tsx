78eedfaf4fddbbbb38f9fceba0bb05798ee226ef
Harshit Shukla <harshitshukla6388@gmail.com>
2025-11-01 22:42:24 +0530

    add the new project

---
+++ apps/excelidraw-frontend/components/RoomCanvas.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { WS_URL } from "@/config";
+import { initDraw } from "@/draw";
+import { useEffect, useRef, useState } from "react";
+import { Canvas } from "./Canvas";
+
+export function RoomCanvas({roomId}: {roomId: string}) {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+    useEffect(() => {
+        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjZGQzNjA1Ny1iMjQ2LTQwOWUtYTI3OS1mZWQyODJlMjgwMWEiLCJpYXQiOjE3NjE1NTM0Nzd9.qqdVXlumf-CfmhVoAr2YF35of4Ucvz14f20xh2P-mfA`)
+
+        ws.onopen = () => {
+            setSocket(ws);
+            const data = JSON.stringify({
+                type: "join_room",
+                roomId
+            });
+            console.log(data);
+            ws.send(data)
+        }
+        
+    }, [])
+   
+    if (!socket) {
+        return <div>
+            Connecting to server....
+        </div>
+    }
+
+    return <div>
+        <Canvas roomId={roomId} socket={socket} />
+    </div>
+}
